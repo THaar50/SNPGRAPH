6184e228a47daa8923cc7626d07ae6fba2de2cdc
THaar50 <haar.tobias@gmail.com>
2022-03-14 01:36:12 +0100

    support for dbsnp files

--- src/main/java/SNPGRAPH/SNPGRAPH.java
+++ src/main/java/SNPGRAPH/SNPGRAPH.java
@@ -6,10 +6,7 @@ package SNPGRAPH;
 
 import org.apache.commons.cli.ParseException;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -38,18 +35,42 @@ public class SNPGRAPH {
 		System.setProperty("snpgraph.dbsnp.datapath", cmdParameters.getStringValue("snpFile"));
 		System.setProperty("snpgraph.resources", "/snpgraph/build/resources/main/");
 
-		//TODO: Read command line parameters from function call (read dbSNP file, read chipSeq file, window close config, window sizes)
-
-		File dir = new File(System.getProperty("snpgraph.chipseq.datapath"));
-		if (dir.isFile()) {
-			newRun(windowSize, dir);
-			System.out.println("Done with " + dir.getName());
+        Database database = new Database();
+		File dbsnpDir = new File(System.getProperty("snpgraph.dbsnp.datapath"));
+		if (dbsnpDir.isFile()) {
+			System.out.println("Importing new SNP data and overwriting existing SNP data...");
+			database.dropTable("snp");
+			prepAndReadInSnpData(dbsnpDir.getAbsolutePath());
 		} else {
-			File[] chipSeqFiles = dir.listFiles();
-			if (chipSeqFiles != null) {
-				for (File chipSeqFile : chipSeqFiles) {
-					newRun(windowSize, chipSeqFile);
-					System.out.println("Done with " + chipSeqFile.getName());
+			File[] dbsnpFiles = dbsnpDir.listFiles();
+			if (dbsnpFiles == null) {
+				System.out.println("No dbSNP file found in " + dbsnpDir.getAbsolutePath());
+				return;
+			}
+			if (dbsnpFiles.length > 1) {
+                System.out.println("Too many dbSNP files found. Please select one.");
+				return;
+			}
+            if (dbsnpFiles.length == 1) {
+				if (database.tableExists("snp")) {
+					System.out.println("Using existing SNP data from database.");
+				} else {
+					database.dropTable("snp");
+					prepAndReadInSnpData(dbsnpFiles[0].getAbsolutePath());
+				}
+            }
+		}
+
+		File chipseqDir = new File(System.getProperty("snpgraph.chipseq.datapath"));
+		if (chipseqDir.isFile()) {
+			newRun(windowSize, chipseqDir, database);
+			System.out.println("Done with " + chipseqDir.getName());
+		} else {
+			File[] chipseqFiles = chipseqDir.listFiles();
+			if (chipseqFiles != null) {
+				for (File chipseqFile : chipseqFiles) {
+					newRun(windowSize, chipseqFile, database);
+					System.out.println("Done with " + chipseqFile.getName());
 				}
 			} else {
 				System.out.println(String.format("No chipSeq files found in %s!", System.getProperty("snpgraph.chipseq.datapath")));
@@ -61,16 +82,15 @@ public class SNPGRAPH {
 	 * Start new run 
 	 */
 	//closeAll
-	public static void newRun(int windowSize, File file) {
+	public static void newRun(int windowSize, File file, Database database) {
 		String file_path = file.getAbsolutePath();
 
 		System.out.println("File: " + file_path);
 		
 		long starttime = System.nanoTime();
-		Database database = new Database();
 
 		// Prepare and read in data with scripts
-		prepAndReadInData(file_path);
+		prepAndReadInChipData(file_path);
 		
 		long readInTime = System.nanoTime();
 		System.out.println("Read in time: " + (readInTime-starttime)/1000000000.0 + " seconds");
@@ -97,43 +117,54 @@ public class SNPGRAPH {
 	 * database table with SQLite .import
 	 */
 
-	public static void prepAndReadInData(String file) {
+	public static void prepAndReadInChipData(String file) {
 		String[] cmd = { "sh", System.getProperty("snpgraph.resources") + "prepChipData.sh", file };
 		String[] cmd2 = { "sh", System.getProperty("snpgraph.resources") + "importChipData.sh" };
-		try {
-			Process p = Runtime.getRuntime().exec(cmd);
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				System.out.println(inputLine);
-			}
-			in.close();
-			p.waitFor();
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-		}
-		
+        runScript(cmd);
 		System.out.println("Importing into database..");
-		
 		// Import data into database with .import script
-		try {
-			Process p = Runtime.getRuntime().exec(cmd2);
-
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					p.getInputStream()));
-			String inputLine;
-			while ((inputLine = in.readLine()) != null) {
-				System.out.println(inputLine);
-			}
-			in.close();
-			p.waitFor();
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-		}
+        runScript(cmd2);
 		System.out.println("Done!");
 	}
+
+    /**
+     * Prepare data with the prepSNPData script for given filename and import into
+     * database table with SQLite .import
+     */
+
+    public static void prepAndReadInSnpData(String file) {
+        String[] cmd = { "sh", System.getProperty("snpgraph.resources") + "prepSNPData.sh", file };
+        String[] cmd2 = { "sh", System.getProperty("snpgraph.resources") + "importSNPData.sh" };
+
+        runScript(cmd);
+        System.out.println("Importing into database..");
+        // Import data into database with .import script
+        runScript(cmd2);
+        System.out.println("Done!");
+    }
+
+    /**
+     * Executes given shell command in a new process.
+     * @param cmd shell command as a list.
+     */
+
+    public static void runScript(String[] cmd) {
+        try {
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    p.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+            p.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 	
 	/**
 	 * Rounds double value to a given decimal place 
